5f835d9298a6a13694d1a2b7af95e75b116e3a13
Manuel Carofiglio <pillscolp@gmail.com>
2022-10-24 00:18:14 +0200

    Initial commit

---
+++ NoSpidersMod.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Modding;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NoSpidersMod
+{
+    public class NoSpidersMod : Mod, ITogglableMod, IMod, Modding.ILogger
+    {
+        public static NoSpidersMod Instance { get; private set; }
+
+        private GameObject _currentSpiderDeterrent;
+
+        public NoSpidersMod()
+            : base(null)
+        {
+            Instance = this;
+        }
+
+        public override string GetVersion()
+        {
+            return "1.0.0";
+        }
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            UnityEngine.SceneManagement.SceneManager.activeSceneChanged += (Scene old, Scene @new) =>
+            {
+                if (!@new.name.StartsWith("Deepnest") || @new.name == "Deepnest_30")
+                {
+                    return;
+                }
+
+                _currentSpiderDeterrent = new GameObject("SpiderDeterrent");
+                _currentSpiderDeterrent.AddComponent<SpiderDeterrent>();
+            };
+        }
+
+        public void Unload()
+        {
+            if (_currentSpiderDeterrent != null)
+            {
+                GameObject.Destroy(_currentSpiderDeterrent);
+            }
+        }
+    }
+}
---
+++ SpiderDeterrent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace NoSpidersMod
+{
+    public class SpiderDeterrent : MonoBehaviour
+    {
+        private GameObject _fgSpiders;
+
+        private void Awake()
+        {
+            StartCoroutine(CheckScene());
+        }
+
+        private IEnumerator CheckScene()
+        {
+            var _fgSpiders = GameObject.Find("fg_spiders");
+            if (_fgSpiders != null)
+            {
+                _fgSpiders.SetActive(false);
+            }
+
+            yield break;
+        }
+
+        private void OnDisable()
+        {
+            _fgSpiders.SetActive(true);
+        }
+    }
+}
